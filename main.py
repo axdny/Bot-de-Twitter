3f3e47596538882936e759ebe830183a0a1c5bec
axdny <73449760+axdny@users.noreply.github.com>
2020-10-27 13:11:25 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tweepy
+import time
+import random
+import sys
+
+sys.path.insert(0, "/Users/Andres/Google Drive/Cursos de Programacion/Python/bot_twitter/credenciales.py")
+sys.path.insert(0, "/Users/Andres/Google Drive/Cursos de Programacion/Python/bot_twitter/listado.py")
+from credenciales import *
+#from listado import *
+
+twitter_API = tweepy.API(auth)
+
+listado_canciones = open ("/Users/Andres/Google Drive/Cursos de Programacion/Python/bot_twitter/listado_canciones.txt", "r")
+leer_canciones = listado_canciones.readlines ()
+
+#for i in canciones:
+#    lyric = random.choice (canciones)
+#    twitter_API.update_status (lyric)
+#    time.sleep (43200)
+
+for i in leer_canciones:
+    seleccion = random.choice (leer_canciones)
+    twitter_API.update_status (seleccion)
+    time.sleep (43200)
+
+print ("Funcionando!!!")
+
+listado_canciones.close()
